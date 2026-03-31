35a61fdd834b7dd65b98d2b1281e028ee19f2554
Cering <gfypm@qq.com>
2026-03-31 18:57:12 +0800

    [cpp][104] Maximum Depth of Binary Tree

---
+++ cpp/104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=104 lang=cpp
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (77.99%)
+ * Likes:    14297
+ * Dislikes: 292
+ * Total Accepted:    4.7M
+ * Total Submissions: 6M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return its maximum depth.
+ * 
+ * A binary tree's maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,null,2]
+ * Output: 2
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 10^4].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        int left_depth = maxDepth(root->left);
+        int right_depth = maxDepth(root->right);
+        return 1 + (left_depth >= right_depth ? left_depth : right_depth);
+    }
+};
+// @lc code=end
+
